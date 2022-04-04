76db1d202f6affa3f65b81e2db4cf82d540185fc
My Grandin <my.grandin@gmail.com>
2022-04-04 12:05:21 +0200

    Added using DataAnnotations

--- Fora Almas Dator/Shared/InterestModel.cs
+++ Fora Almas Dator/Shared/InterestModel.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Fora_Almas_Dator.Shared
 {
-    internal class InterestModel
+    public class InterestModel
     {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public List<ThreadModel> Threads { get; set; }
+
+        // Relations
+        public List<UserInterestModel> UserInterests { get; set; }
+
+        [ForeignKey(nameof(User))]
+        public int? UserId { get; set; }
+        public UserModel? User { get; set; }
     }
 }
