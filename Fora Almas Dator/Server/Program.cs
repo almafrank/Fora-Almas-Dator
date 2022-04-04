f9b68134396f6b779165d531a431d0db44639704
almafrank <92314215+almafrank@users.noreply.github.com>
2022-04-04 13:05:57 +0200

    Update content

--- Fora Almas Dator/Server/Program.cs
+++ Fora Almas Dator/Server/Program.cs
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
 
