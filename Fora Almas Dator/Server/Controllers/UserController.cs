78d12dde5e6aa105f924737ba6cad03b60eecefd
almafrank <92314215+almafrank@users.noreply.github.com>
2022-04-04 14:53:10 +0200

    Add changes

--- Fora Almas Dator/Server/Controllers/UserController.cs
+++ Fora Almas Dator/Server/Controllers/UserController.cs
@@ -2,42 +2,49 @@
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace Fora Almas Dator.Server.Controllers
+namespace Fora_Almas_Dator.Server.Controllers
 {
-    [Route("api/[controller]")]
-[ApiController]
-public class UserController : ControllerBase
-{
-    // GET: api/<UserController>
-    [HttpGet]
-    public IEnumerable<string> Get()
+        [Route("api/[controller]")]
+        [ApiController]
+    public class UserController : ControllerBase
     {
-        return new string[] { "value1", "value2" };
-    }
+        private readonly AppDbContext _dbContext;
 
-    // GET api/<UserController>/5
-    [HttpGet("{id}")]
-    public string Get(int id)
-    {
-        return "value";
-    }
+        public UserController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        // GET: api/<UserController>
+        [HttpGet]
+        public List<UserModel> GetUserInterest(int id)
+        {
+            var user = _dbContext.Users.All(x => x.Id == id);
+           return _dbContext.
+        }
 
-    // POST api/<UserController>
-    [HttpPost]
-    public void Post([FromBody] string value)
-    {
-    }
+        // GET api/<UserController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
 
-    // PUT api/<UserController>/5
-    [HttpPut("{id}")]
-    public void Put(int id, [FromBody] string value)
-    {
-    }
+        // POST api/<UserController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
 
-    // DELETE api/<UserController>/5
-    [HttpDelete("{id}")]
-    public void Delete(int id)
-    {
+        // PUT api/<UserController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UserController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
     }
-}
 }
