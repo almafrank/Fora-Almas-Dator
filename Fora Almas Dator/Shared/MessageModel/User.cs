36c3c924d34a36ae51f6e4dd186741295598c9bc
almafrank <92314215+almafrank@users.noreply.github.com>
2022-04-04 11:44:51 +0200

    Add user prop

---
+++ Fora Almas Dator/Shared/MessageModel/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fora_Almas_Dator.Shared.MessageModel
+{
+    internal class User
+    {
+    }
+}
